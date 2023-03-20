2f18715275f73215de5c551f7ffcb98a59084689
Malindu-Upendra-web3genes <124072078+Malindu-Upendra-web3genes@users.noreply.github.com>
2023-03-20 09:43:46 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/cicd-backend/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/cicd-backend/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/cicd-backend/deploy.log
+cd /home/ec2-user/cicd-backend >> /home/ec2-user/cicd-backend/deploy.log
 
 echo 'npm install' >> /home/ec2-user/cicd-backend/deploy.log 
 npm install >> /home/ec2-user/cicd-backende/deploy.log
