734808f779bb48f4edf0a72e53e541876812c9ab
USER <malindu@web3genes.com>
2023-04-12 13:57:11 +0530

    change script files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/cicd-backend/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/cicd-backend/deploy.log
-cd /home/ec2-user/cicd-backend >> /home/ec2-user/cicd-backend/deploy.log
+echo 'cd /home/ec2-user/securedoc/nodejs-server-cicd' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
+cd /home/ec2-user/securedoc/cicd-backend >> /home/ec2-user/securedoc/cicd-backend/deploy.log
 
-echo 'npm install' >> /home/ec2-user/cicd-backend/deploy.log 
-npm i --legacy-peer-deps >> /home/ec2-user/cicd-backend/deploy.log
+echo 'npm install' >> /home/ec2-user/securedoc/cicd-backend/deploy.log 
+npm i --legacy-peer-deps >> /home/ec2-user/securedoc/cicd-backend/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/cicd-backend/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/cicd-backend/deploy.log
-pm2 restart backend >> /home/ec2-user/cicd-backend/deploy.log
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
+pm2 restart backend >> /home/ec2-user/securedoc/cicd-backend/deploy.log
