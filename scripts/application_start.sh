b03172f001990ebb833a5a3f2b429bc043fd5a05
USER <malindu@web3genes.com>
2023-04-12 14:01:01 +0530

    changed application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,5 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/securedoc/cicd-backend/deploy.log
-pm2 restart backend >> /home/ec2-user/securedoc/cicd-backend/deploy.log
+sudo pm2 restart backend >> /home/ec2-user/securedoc/cicd-backend/deploy.log
+sudo pm2 save >> /home/ec2-user/securedoc/cicd-backend/deploy.log
